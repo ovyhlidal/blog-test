73eba95951e1bb43fe5a7e0f7c81053271801f2c
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-02 14:32:52 +0100

    no message

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -39,7 +39,6 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     redirect_to root_path
-
   end
 
   private
