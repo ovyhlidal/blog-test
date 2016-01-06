25561e8e5199ab110b4300072ec91bf2af7c6dae
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-06 22:00:40 +0100

    paperclip added to users

--- blog/db/migrate/20160106201254_add_paperclip_to_user.rb
+++ blog/db/migrate/20160106201254_add_paperclip_to_user.rb
@@ -1,6 +1,12 @@
 class AddPaperclipToUser < ActiveRecord::Migration
 
-  add_attachment :users, :avatar
-  def change
+  def self.up
+    change_table :users do |t|
+      t.attachment :avatar
+    end
+  end
+
+  def self.down
+    drop_attached_file :users, :avatar
   end
 end
