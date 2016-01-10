f5cc2cb85dd57005591dde779ecc148957e847a2
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-10 12:53:37 +0100

    after sign up -> 'posts#index'

--- blog/app/controllers/registrations_controller.rb
+++ blog/app/controllers/registrations_controller.rb
@@ -2,10 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_inactive_sign_up_path_for(resource)
-    root_path
+    'posts#index'
   end
 
   def after_sign_up_path_for(resource)
-    root_path
+    'posts#index'
   end
 end
