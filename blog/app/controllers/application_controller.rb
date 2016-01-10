7d917dd130cc3a1e3ec6cc47d718a6fa6e0cc08a
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-10 11:11:55 +0100

    after registration redirect to root path ]

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
   end
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
 end
