a87f490111a520c98c72ae443ed301c9d59a4238
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-10 11:56:44 +0100

    devise mailer

--- blog/config/environments/production.rb
+++ blog/config/environments/production.rb
@@ -79,7 +79,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'https://blooming-caverns-6833.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.smtp_settings = {
       :port      => 587,
       :address    => "smtp.gmail.com",
