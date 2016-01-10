cc9ff257c073ec565a4579570cce25f452fc8f75
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-10 13:28:06 +0100

    no message

--- blog/app/controllers/registrations_controller.rb
+++ blog/app/controllers/registrations_controller.rb
@@ -2,10 +2,13 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_inactive_sign_up_path_for(resource)
+    # binding.pry
+    # puts "========= after_inactive_sign_up_path_for"
     'posts#index'
   end
 
   def after_sign_up_path_for(resource)
+    # puts "========= after_sign_up_path_for"
     'posts#index'
   end
 end
